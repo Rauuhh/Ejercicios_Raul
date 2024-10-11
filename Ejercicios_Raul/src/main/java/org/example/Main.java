a16becebf017bf2ffab2af87b212ddd6e5fa43e4
A701\1dam <raildetrenes@gmail.com>
2024-10-11 16:26:13 +0200

    no message

--- Ejercicios_Raul/src/main/java/org/example/Main.java
+++ Ejercicios_Raul/src/main/java/org/example/Main.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import java.util.Scanner;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -18,18 +16,22 @@ public class Main {
                 /*Ejercicio3 ejercicio3 = new Ejercicio3();
                 ejercicio3.ejercicio3();*/
                     /*Operadores operadores = new Operadores();
-                    operadores.operadores();
-                    operadores.asignacion();
-                    operadores.comparacion();
-                    operadores.logicos();*/
-                        /*Ejercicio4 ejercicio4 = new Ejercicio4();
-                        ejercicio4.ejercicio4();*/
-                           /*Ejercicio5 ejercicio5 = new Ejercicio5();
-                            ejercicio5.ejercicio5();*/
-                                /*Ejercicio6 ejercicio6 = new Ejercicio6();
-                                ejercicio6.ejercicio6();*/
-                                         Practica4 practica4 = new Practica4();
-                                         practica4.practica4();
+                      operadores.operadores();
+                          operadores.asignacion();
+                             operadores.comparacion();
+                                operadores.logicos();*/
+                                    /*Ejercicio4 ejercicio4 = new Ejercicio4();
+                                    ejercicio4.ejercicio4();*/
+                                       /*Ejercicio5 ejercicio5 = new Ejercicio5();
+                                        ejercicio5.ejercicio5();*/
+                                            /*Ejercicio6 ejercicio6 = new Ejercicio6();
+                                            ejercicio6.ejercicio6();*/
+                                                /*Practica4 practica4 = new Practica4();
+                                                 practica4.practica4();*/
+                                                    /*BateriaUD2 bateria2 = new BateriaUD2();
+                                                    bateria2.bateria2();*/
+                                                        Practica1 practica1 = new Practica1();
+                                                        practica1.practica1();
 
     }
 }
---
+++ Ejercicios_Raul/src/main/java/org/example/Practica1.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Practica1 {
+    public void practica1() {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingresa tu fecha de nacimiento (dd/mm/yyyy): ");
+        String fecha = entrada.nextLine();
+        fecha=fecha.replaceAll("[^\\p{Alpha}]+","");
+
+        String dia = fecha.substring(0,2);
+        String mes = fecha.substring(2,4);
+        String anio = fecha.substring(4,8);
+                     int dia2 =  Integer.parseInt(dia);
+                     int mes2 =  Integer.parseInt(mes);
+                     int ano2 =  Integer.parseInt(anio);
+        if (dia2 < 1 || dia2 > 31 || mes2 < 1 || mes2 > 12 || ano2 < 1)
+        {
+            System.out.println("Error: La fecha de nacimiento no es correcta");
+            System.exit(0);
+        }
+        int result = dia2+mes2+ano2;
+        String resultado = String.valueOf(result);
+        System.out.println(dia2 + "+" + mes + "+" + ano2 + " = " + result);
+
+        int digito1 = Integer.parseInt(resultado.substring(0,1));
+        int digito2 = Integer.parseInt(resultado.substring(1,2));
+        int digito3 = Integer.parseInt(resultado.substring(2,3));
+        int digito4 = Integer.parseInt(resultado.substring(3,4));
+        int result2 = digito1+digito2+digito3+digito4;
+        System.out.println(digito1 + "+" + digito2 + "+" + digito3 + "+" + digito4 + " = " + result2);
+        System.out.println("Tu numero de la suerte es el: " + result2);
+
+
+
+    }
+}
