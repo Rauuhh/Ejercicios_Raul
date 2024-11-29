6b1ac9404b0aca8757eb8968eaf8429d1eb4a666
Rauuhh <raildetrenes@gmail.com>
2024-11-29 23:37:46 +0100

    Update Loteria.java

--- Ejercicios_Raul/src/main/java/org/example/Loteria.java
+++ Ejercicios_Raul/src/main/java/org/example/Loteria.java
@@ -1,1 +1,93 @@
+package org.example;
 
+import java.util.Random;
+import java.util.Scanner;
+
+public class Loteria {
+    public void loteria()
+    {
+        Scanner entrada = new Scanner(System.in) ;
+        Random aleatorio = new Random();
+        System.out.println("Introduce el boleto con el formato N-N-N-N-N-N/R Numeros entre el 1 - 49 y el reintegro entre 0 - 9");
+        String boleto = entrada.nextLine();
+
+        if (!boleto.matches("(\\d{1,2}-){5}\\d{1,2}/\\d")) {
+            System.out.println("Formato incorrecto.");
+            return;
+        }
+
+        String partes [] = boleto.split("[-/]");
+        int numerosUsuario[] = new int [6];
+        int reintegroUsuario = Integer.parseInt(partes[6]);
+
+        for (int i = 0; i < 6; i++) {
+            numerosUsuario[i] = Integer.parseInt(partes[i]);
+            if (numerosUsuario[i] < 1 || numerosUsuario[i] > 49) {
+                System.out.println("Numero fuera de rango");
+                return;
+            }
+        }
+
+        if (reintegroUsuario < 0 || reintegroUsuario > 9) {
+            System.out.println("Reintegro fuera del rango 0-9");
+            return;
+        }
+
+        int numerosGanadores [] = new int[6];
+        for (int i = 0;i < 6;i++){
+            int num;
+            boolean repetido;
+            do {
+                num = aleatorio.nextInt(49) + 1;
+                repetido = false;
+                for (int j = 0; j < i; j++) {
+                    if (numerosGanadores[j] == num){
+                        repetido = true;
+                        break;
+                    }
+                }
+            } while (repetido);
+            numerosGanadores[i] = num;
+        }
+        int numeroComplementario;
+        boolean repetido;
+        do {
+            numeroComplementario = aleatorio.nextInt(49) + 1;
+            repetido = false;
+            for (int i = 0; i < 6; i++) {
+                if (numerosGanadores[i] == numeroComplementario) {
+                    repetido = true;
+                    break;
+                }
+            }
+        } while (repetido);
+        int reintegroGanador = aleatorio.nextInt(10);
+        int contador = 0;
+        if (reintegroGanador == reintegroUsuario)
+        {
+           contador = 1;
+        }
+
+        System.out.println("Numeros ganadores: ");
+        for (int i = 0;i < 6; i++){
+            System.out.print(numerosGanadores[i] + " ");
+        }
+        System.out.print("");
+        System.out.println("Numero complementario: " +numeroComplementario);
+        System.out.println("Reintegro: "+reintegroGanador);
+
+        int aciertos = 0;
+        for (int i = 0;i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (numerosUsuario[i] == numerosGanadores[j]) {
+                    aciertos++;
+                }
+            }
+        }
+        System.out.println("Has acertado " +aciertos+ " numeros");
+        if ( contador == 1)
+        {
+            System.out.println("Has acertado el reintregro");
+        }
+    }
+}
