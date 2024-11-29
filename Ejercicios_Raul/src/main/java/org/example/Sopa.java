a6a133b1e893a73cec0e7dba2a5c9c83e11b743f
Rauuhh <raildetrenes@gmail.com>
2024-11-29 23:48:34 +0100

    Update Sopa.java

--- Ejercicios_Raul/src/main/java/org/example/Sopa.java
+++ Ejercicios_Raul/src/main/java/org/example/Sopa.java
@@ -1,106 +1,86 @@
 package org.example;
-
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Sopa {
-    public void sopa()
-    {
 
+    public void sopa() {
         Scanner entrada = new Scanner(System.in);
-        System.out.println("Escribe el numero de filas");
-        int filal = entrada.nextInt();
-        System.out.println("Escribe el numero de columnas");
-        int columnal = entrada.nextInt();
-        String matriz[][] = new String [filal][columnal];
-        for (int i = 0; i < matriz.length ; i++) {
-            System.out.println("Introduce valores de la fila");
-            String fila = entrada.next();
-            String fila2[] = fila.split("");
-            if ( columnal != fila2.length )
-            {
-                System.out.println("Longitud de linea erronea, vuelvela a poner");
-                i--;
-                continue;
-            }
-            if (!fila.matches("[a-zA-Z]+"))
-            {
-                System.out.println("No cumple el formato correcto, solo se pueden letras");
-                i--;
-                continue;
-            }
-            for (int j = 0; j < matriz[0].length; j++) {
-                matriz[i][j] = fila2[j];
-            }
+        int fila = 0;
+        int columna = 0;
+        while (true) {
+            try {
+                System.out.println("Introduce el número de filas: ");
+                fila = entrada.nextInt();
+                System.out.println("Introduce el número de columnas: ");
+                columna = entrada.nextInt();
 
-        }
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                System.out.print(matriz[i][j] + " ");
-            }
-            System.out.print("\n");
-        }
-
-        int posi = 0;
-        int posj = 0;
-        System.out.println("Introduce la palabra a buscar");
-        String buscar = entrada.next();
-        String[] buscar2 = buscar.split("");
-        for (int i = posi; i < matriz.length ; i++)
-        {
-            for (int j = 0; j < buscar2.length; j++)
-            {
-                for (int k = 0; k < buscar2.length; k++)
-                {
-                    if (!matriz[i][j+k].equals(buscar2[k]))
-                    {
-                        break;
-                    }
-                    if (k == buscar2.length) {
-                        posi = j;
-                        posj = j;
-                        break;
-                    }
-                }
-
-            }
-        }
-        if (posi != -1 && posj != -1) {
-            System.out.println("Encontrada!!! Comienza en la posición" + posi + ", columna " + posj);
-            return;
-        }
-
-         posi = -1;
-         posj = -1;
-        for (int j = posj; j < matriz[0].length ; j++)
-        {
-            for (int i = 0; i < buscar2.length; i++)
-            {
-                for (int k = 0; k < buscar2.length; k++)
-                {
-                    if (!matriz[i - k][j].equals(buscar2[i]))
-                    {
-                        break;
-                    }
-                    if (k == buscar2.length) {
-                        posi = i;
-                        posj = j;
-                        break;
-                    }
-                }
-                if (posi != -1 && posj != -1)
-                {
+                if (fila > 0 && columna > 0) {
                     break;
                 }
+                System.out.println("Las filas y columnas deben ser mayores que 0.");
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada no válida. Solo se permiten números.");
+                return;
             }
         }
-        if (posi != -1 && posj != -1)
-        {
-            System.out.println("Encontrada!!! Comienza en la posición" + posi + ", columna " + posj);
-        } else {
-            System.out.println("Palabra no encontrada en la sopa de letras.");
+        String[][] sopa = new String[fila][columna];
+        for (int i = 0; i < fila; i++) {
+            while (true) {
+                System.out.print("Introduce las letras de la fila " + (i + 1) + ": ");
+                String linea = entrada.next();
+
+                if (linea.length() == columna && linea.matches("[a-zA-Z]+")) {
+                    for (int j = 0; j < columna; j++) {
+                        sopa[i][j] = "" + linea.charAt(j); 
+                    }
+                    break;
+                }
+                System.out.println("La fila debe tener " + columna + " letras.");
+                return;
+            }
+        }
+        System.out.println("\nSopa de letras:");
+        for (int i = 0; i < sopa.length; i++) {
+            for (int j = 0; j < sopa[i].length; j++) {
+                System.out.print(sopa[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.print("Introduce la palabra a buscar: ");
+        String palabra = entrada.next();
+        boolean encontrada = false;
+
+        for (int i = 0; i < fila && !encontrada; i++) {
+            String filaSopa = "";
+            for (int j = 0; j < columna; j++) {
+                filaSopa += sopa[i][j]; 
+            }
+
+            int pos = filaSopa.indexOf(palabra);
+            if (pos != -1) {
+                System.out.println("Palabra encontrada en la posicion " + (i) + " " + (pos));
+                encontrada = true;
+            }
         }
 
+        if (!encontrada) {
+            for (int j = 0; j < columna && !encontrada; j++) {
+                String columnaSopa = "";
+                for (int i = 0; i < fila; i++) {
+                    columnaSopa += sopa[i][j];  
+                }
 
-
+                int pos = columnaSopa.indexOf(palabra);
+                if (pos != -1) {
+                    System.out.println("Palabra encontrada en la posicion " + (j) + " " + (pos));
+                    encontrada = true;
+                }
+            }
+        }
+        if (!encontrada) {
+            System.out.println("No se ha encontrado la palabra.");
+        }
     }
 }
+
